9e255a9e3bafe57ac34d2d1676cd0e9366c4099f
allynmuzhixu <74119280+allynmuzhixu@users.noreply.github.com>
2020-12-11 22:13:43 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-#%% Learn
-
+#%% Initialize
 from dqn_agent import DQNAgent
 from tetris import Tetris
 from datetime import datetime
@@ -37,12 +36,10 @@ def experience(render=False, max_steps=200):
         agent.add_to_memory(current_state, next_states[best_action], reward, done)
         current_state = next_states[best_action]
         steps += 1
-    
-
 
 def validate(num_reps, validate_render=True, max_steps=200):
-    temp_scores = []
-    temp_steps = []
+    val_scores_record = []
+    val_steps_record = []
     
     for rep in range(num_reps):
         if validate_render and not rep:
@@ -65,32 +62,33 @@ def validate(num_reps, validate_render=True, max_steps=200):
                     break
     
             reward, done = env.play(best_action[0], best_action[1], render=render,
-                                    render_delay=render_delay)
+                                    render_delay=None)
             
             current_state = next_states[best_action]
             steps += 1
         
-        temp_scores.append(env.get_game_score())
-        temp_steps.append(steps)
+        val_scores_record.append(env.get_game_score())
+        val_steps_record.append(steps)
     
-    mean_score = np.mean(temp_scores)
-    mean_steps = np.mean(temp_steps)
+    mean_score = np.mean(val_scores_record)
+    mean_steps = np.mean(val_steps_record)
     
     print()
     print("---------------")
     print("Validation")
     print()
+    print("Max Score:\t" + str(max(val_scores_record)))
     print("Game Score:\t" + str(mean_score) )
     print("Steps:\t\t" + str(mean_steps) )
     print("---------------")
     print()
     
-    return mean_score, mean_steps
+    return val_scores_record, val_steps_record
 
 
+def dqn():        
+    SAVE_MODEL_IF_SCORE_GREATER_THAN = 100
 
-
-def dqn():
     for episode in tqdm(range(1,episodes+1)):
 
         # Game
@@ -105,14 +103,32 @@ def dqn():
         # Validate
         if episode % validate_every == 0:
             val_score, val_step = validate(num_val_reps,validate_render,max_steps )
-            val_scores.append(val_score)
-            val_steps.append(val_step)
+            val_scores.append(np.mean(val_score))
+            val_steps.append(np.mean(val_step))
     
         wall_time.append(time())
         
+        # Logs
+        if log_every and episode and episode % log_every == 0:
+            avg_score = mean(scores[-log_every:])
+            min_score = min(scores[-log_every:])
+            max_score = max(scores[-log_every:])
+            val_avg_score = mean(val_score)
+            val_min_score = min(val_score)
+            val_max_score = max(val_score)
+            
+            log.log(episode, avg_score=avg_score, min_score=min_score,
+                    max_score=max_score, val_avg_score=val_avg_score,
+                    val_min_score=val_min_score, val_max_score=val_max_score,
+                    wall_time=time() - start_time)
+            
+            if val_max_score > SAVE_MODEL_IF_SCORE_GREATER_THAN:
+                # save_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+                agent.model.save(f'models/tetris-epsilon={epsilon}-epsilon_min={epsilon_min}-epsilon_stop_episode={epsilon_stop_episode}-episode={episode}-score-{val_max_score}-{datetime.now().strftime("%Y%m%d-%H%M%S")}.h5')
+        
         
 
-
+#%% Models
 env = Tetris()
 episodes = 2000
 max_steps = 200
@@ -125,27 +141,27 @@ training_batch_size = 512
 memory_batch_size = 512
 epochs = 1
 replay_start_size = 2048
-
-# render_every = None
-# log_every = 50
-
 train_every = 1
-
-# render_delay = None
-
+                                 
 validate_every = 25
 num_val_reps = 5
 validate_render = True
-
-n_neurons = [64,64]
+                                 
+n_neurons = [32,32]
 activations = ['relu', 'relu', 'linear']
 add_batch_norm = True
-
+use_target_model = False
+update_target_every = None
+                                 
 agent = DQNAgent(env.get_state_size(),
                  n_neurons=n_neurons, activations=activations, add_batch_norm=add_batch_norm,
-                 epsilon=epsilon, epsilon_min=epsilon_min,
+                 epsilon=epsilon, epsilon_min=epsilon_min,use_target_model=use_target_model,
+                 update_target_every=update_target_every,
                  epsilon_stop_episode=epsilon_stop_episode, mem_size=mem_size,
                  discount=discount, replay_start_size=replay_start_size)
+
+log_dir = f'logs/tetris-epsilon={epsilon}-epsilon_min={epsilon_min}-epsilon_stop_episode={epsilon_stop_episode}-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+log = CustomTensorBoard(log_dir=log_dir)
 
 start_time = time()
 wall_time = []
@@ -154,33 +170,6 @@ val_scores = []
 val_steps = []
 
 dqn()
-
-
-    
-
-#%% Save
-save_time = datetime.now().strftime("%Y%m%d-%H%M%S")
-
-results = { 'wall_time' : wall_time,
-            'scores' : scores,
-            'val_scores' : val_scores,
-            'val_steps' : val_steps }
-
-model_description = 'basic'
-
-with open(f'./models/tetris-nn-{model_description}-{save_time}.pkl', 'wb') as f:
-    pickle.dump(results, f, pickle.HIGHEST_PROTOCOL)
-    
-agent.model.save(f'./models/tetris-nn-{model_description}-{save_time}.h5')
-
-with open(f'./models/tetris-nn-{model_description}-{save_time}.txt', 'w') as f:
-    print('', file=f)
-
-
-# load_file = './models/tetris-nn-{model_description}-{save_time}'
-# with open('./models/' + load_file + '.pkl, 'rb') as f:
-#     results = pickle.load(f)
-# model = tf.keras.models.load_model('./models/' + load_file + '.h5')
 
 
 #%% Analyze
